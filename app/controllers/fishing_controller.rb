351746e9fb3bfbe7904ce2d88b59942247e005b5
Marcus Maday <marcus.maday@gmail.com>
2015-08-31 23:42:52 -0500

    Factor out different pieces to cache

--- app/controllers/fishing_controller.rb
+++ app/controllers/fishing_controller.rb
@@ -5,6 +5,7 @@ class FishingController < ApplicationController
   # GET /fishing.json
   def index
     @user_catches = getUserCatches()
+    @keepers = getKeepers()
     logger.debug "user_catches=" + @user_catches.to_s
 
     @fish_types = FishType.all if @fish_type.nil?
@@ -17,64 +18,89 @@ class FishingController < ApplicationController
 
   def catch
     gps_info = getGPSInfo()
-    @user_catches = getUserCatches()
+    user_catches = getUserCatches()
     #Create the new catch and save it to get the id
-    @catch = Catch.new({ user_id: params[:user_id], fish_type_id: params[:fish_type_id], lat: gps_info[:lat], lon: gps_info[:lon], date: Time.now })
-    @catch.save
+    cat = Catch.new({ user_id: params[:user_id], fish_type_id: params[:fish_type_id], lat: gps_info[:lat], lon: gps_info[:lon], date: Time.now })
+    cat.save
 
-    if @user_catches[@catch.user_id].nil?
-      @user_catches[@catch.user_id] = {}
-      @user_catches[@catch.user_id][@catch.fish_type_id] = 0
-    elsif @user_catches[@catch.user_id][@catch.fish_type_id].nil?
-      @user_catches[@catch.user_id][@catch.fish_type_id] = 0
+    if user_catches[cat.user_id].nil?
+      user_catches[cat.user_id] = {}
+      user_catches[cat.user_id][cat.fish_type_id] = 0
+    elsif user_catches[cat.user_id][cat.fish_type_id].nil?
+      user_catches[cat.user_id][cat.fish_type_id] = 0
     end
-    @user_catches[@catch.user_id][@catch.fish_type_id] += 1
-    @notice_string = User.find(params[:user_id]).nickname + " caught a " + FishType.find(params[:fish_type_id]).name + " <a href='/fishing/keep?catch_id=" + @catch.id.to_s + "'>Mark as Keeper</a>|<a href='/fishing/undo?catch_id=" + @catch.id.to_s + "'>Undo Catch</a>"
+    user_catches[cat.user_id][cat.fish_type_id] += 1
+    user_catches[cat.user_id][:points] += FishType.find(cat.fish_type_id).point_value
+    notice_string = User.find(params[:user_id]).nickname + " caught a " + FishType.find(params[:fish_type_id]).name + " <a href='/fishing/keep?catch_id=" + cat.id.to_s + "'>Mark as Keeper</a>|<a href='/fishing/undo?catch_id=" + cat.id.to_s + "'>Undo Catch</a>"
 
-    Rails.cache.write(:user_catches, @user_catches, expires_in: 1.minutes)
+    Rails.cache.write(:user_catches, user_catches, expires_in: getExpiration())
 
-    redirect_to({action: "index"}, alert: @notice_string.html_safe)
+    redirect_to({action: "index"}, alert: notice_string.html_safe)
   end
 
   def keep
-    @user_catches = getUserCatches()
+    keepers = getKeepers()
 
     cat = Catch.find(params[:catch_id])
     cat.kept = true
     cat.save
-    logger.debug "WHATEVER1=" + @user_catches.to_s
-    @user_catches[:keepers][cat.fish_type_id] = @user_catches[:keepers][cat.fish_type_id] + 1
-    logger.debug "WHATEVER2=" + @user_catches.to_s
+    keepers[cat.fish_type_id] += 1
 
-    @notice_string = "Marked as a keeper"
+    notice_string = "Marked as a keeper"
 
-    Rails.cache.write(:user_catches, @user_catches, expires_in: 1.minutes)
+    Rails.cache.write(:keepers, keepers, expires_in: getExpiration())
 
-    redirect_to({action: "index"}, alert: @notice_string.html_safe)
+    redirect_to({action: "index"}, alert: notice_string.html_safe)
 
   end
 
   def getUserCatches
-    Rails.cache.fetch(:user_catches, expires_in: 1.minutes) do
+    Rails.cache.fetch(:user_catches, expires_in: getExpiration()) do
       user_catches = {}
-      user_catches[:keepers] = {}
-      @fish_types = FishType.all if @fish_type.nil?
-      @fish_types.each do |fish_type|
-        user_catches[:keepers][fish_type.id] = 0
+      keepers = getKeepers()
+      point_values = getPointValues()
+      users = User.all
+      users.each do |user|
+        user_catches[user.id] = {}
+        user_catches[user.id][:points] = 0
       end
       catches = Catch.where('date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
       catches.each do |cat|
         logger.debug "cat=" + cat.to_s
-        if !(user_catches.has_key?(cat.user_id))
-          user_catches[cat.user_id] = {}
+        if !(user_catches[cat.user_id].has_key?(cat.fish_type_id))
           user_catches[cat.user_id][cat.fish_type_id] = 0
         end
         user_catches[cat.user_id][cat.fish_type_id] += 1
+        user_catches[cat.user_id][:points] += point_values[cat.fish_type_id]
         if cat.kept == true
-          user_catches[:keepers][cat.fish_type_id] += 1
+          keepers[cat.fish_type_id] += 1
         end
       end
+      Rails.cache.write(:keepers, keepers, expires_in: getExpiration())
       user_catches
     end
- end
+  end
+  def getKeepers
+    Rails.cache.fetch(:keepers, expires_in: getExpiration()) do
+      keepers = {}
+      fish_types = FishType.all
+      fish_types.each do |fish_type|
+        keepers[fish_type.id] = 0
+      end
+      keepers
+    end
+  end
+  def getPointValues
+    Rails.cache.fetch(:point_values, expires_in: getExpiration()) do
+      point_values = {}
+      fish_types = FishType.all
+      fish_types.each do |fish_type|
+        point_values[fish_type.id] = fish_type.point_value
+      end
+      point_values
+    end
+  end
+  def getExpiration
+    return 1.minutes
+  end
 end
